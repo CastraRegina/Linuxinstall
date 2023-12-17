f49fceefd71234853c185a8620a522c07a74cd1f
fk <castraregina@gmx.net>
2023-12-17 19:09:19 +0100

    generalize makeSnapshotDaily

--- 02_setup_user/HOME_root/bin/010_makeSnapshotDaily.sh
+++ 02_setup_user/HOME_root/bin/010_makeSnapshotDaily.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# example for arguments $1 and $2:
+# example call: 010_makeSnapshotDaily.sh /home /home/snapshots 
 SUBVOLUME="/home"
 SNAPSHOT_DIR="/home/snapshots"
 
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
+# extract values from arguments:
+SUBVOLUME="$1"
+SNAPSHOT_DIR="$2"
 
 snapshot_day=$(date +%Y%m%d)
 snapshot_time=$(date +%H%M%S)
@@ -23,7 +33,7 @@ fi
 # check if for today already a snapshot exists
 ls -d1 ${snapshot_name_day}* >/dev/null 2>&1
 if [ $? -eq 0 ] ; then
-  echo "For today snapshot already exists, ${snapshot_name_day}"
+  echo "For today snapshot already exists: ${snapshot_name_day}"
 else
   btrfs subvolume snapshot -r ${SUBVOLUME} ${snapshot_name_day_time} 
 fi
