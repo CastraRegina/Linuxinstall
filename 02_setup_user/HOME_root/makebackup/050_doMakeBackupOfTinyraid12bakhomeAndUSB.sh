072e50d1320dafc5f085e83404e86e0c1c9b499f
fk <castraregina@gmx.net>
2025-06-09 19:34:32 +0200

    refactor and use latest commandline api of rdiff-backup

--- 02_setup_user/HOME_root/makebackup/050_doMakeBackupOfTinyraid12bakhomeAndUSB.sh
+++ 02_setup_user/HOME_root/makebackup/050_doMakeBackupOfTinyraid12bakhomeAndUSB.sh
@@ -2,17 +2,40 @@
 
 SRCDIR="/mnt/tinyraid1"
 SNAPSHOTDIR="/mnt/tinyraid1/snapshots"
-DESTDIRS="/mnt/bakhome/data/mlc05/backup_tinyraid1"
-DESTDIRS="${DESTDIRS} /media/fk/eaf1e9fa-e085-4ed6-87ce-bd5f924cdf25/data"
-DESTDIRS="${DESTDIRS} /media/fk/6153ad40-0940-4118-9562-5a17286a5591/data"
-DESTDIRS="${DESTDIRS} /media/fk/225393ec-cae0-433a-ac18-749dcfe9e980/data"
-DESTDIRS="${DESTDIRS} /media/fk/7e8a091f-01fa-428f-8c84-2924bfe71956/data"
+DESTDIRS=(
+  # Local backup destination
+  "/mnt/bakhome/data/mlc05/backup_tinyraid1"
+  # USBsticks
+  "/media/fk/eaf1e9fa-e085-4ed6-87ce-bd5f924cdf25/data"
+  "/media/fk/6153ad40-0940-4118-9562-5a17286a5591/data"
+  "/media/fk/225393ec-cae0-433a-ac18-749dcfe9e980/data"
+  "/media/fk/7e8a091f-01fa-428f-8c84-2924bfe71956/data"
+  # USBHDDs
+  "/media/fk/069cab59-bb05-4c98-bcda-d45cff2b1ddd/data"
+)
 
-for DESTDIR in ${DESTDIRS} ; do
-  if [ -e ${DESTDIR} ] ; then
+
+echo "==========================================================================="
+  echo rdiff-backup info
+  rdiff-backup info
+echo "==========================================================================="
+
+
+for DESTDIR in "${DESTDIRS[@]}" ; do
+  if [ -d ${DESTDIR} ] ; then
+    echo "==========================================================================="
     df -h ${DESTDIR}
-    time rdiff-backup --exclude ${SNAPSHOTDIR} --include ${SRCDIR} --exclude '**' / ${DESTDIR}
+    echo "==========================================================================="
+
+    # Using rdiff-backup with --new and --api-version 201 ensures compatibility with the latest API version and enables new features.
+    echo "rdiff-backup --new --api-version 201 backup --exclude ${SNAPSHOTDIR} --include ${SRCDIR} --exclude '**' / ${DESTDIR}"
+    time  rdiff-backup --new --api-version 201 backup --exclude ${SNAPSHOTDIR} --include ${SRCDIR} --exclude '**' / ${DESTDIR}
+
+    echo "==========================================================================="
     df -h ${DESTDIR}
+    echo "==========================================================================="
+
+    echo "==========================================================================="
     echo "diff -rq ${SRCDIR} ${DESTDIR}${SRCDIR}"
     diff -rq ${SRCDIR} ${DESTDIR}${SRCDIR}
     echo "==========================================================================="
